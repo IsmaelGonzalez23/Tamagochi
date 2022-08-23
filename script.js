2c13d9b97c975c1e7c3cd07455e71ed6e4c16bd6
ismaelgonzalez23 <ismaelgonzalez.rios@gmail.com>
2022-08-23 06:40:36 -0500

    added javascript functionality

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-class healthBar {
-    constructor(element, initialValue = 100) {
-        this.fillElement = element.querySelector('#health-bar-fill');
+//Your pets hunger!
+function updateHealth(healthBar,value) {
+    const healthBarFill = healthBar.querySelector(".health-bar-fill");
 
-        this.setValue(initialValue);
-    }
-    setValue (newValue) {
-        if (newValue < 0) {
-            newValue = 0;
-        }
-        if (newValue > 100) {
-            newValue = 100;
-        }
+const stages = healthBarFill.dataset.stages
+    .split(",")
+    .map((stage) => stage.split(":"));
 
-        this.value = newValue;
-        this.update();
-    }
+    stages.sort((stageA, stageB) => {
+        return stageA[0] - stageB[0]
+    });
+
+    healthBarFill.style.width = `${value}%`;
 }
+
+    updateHealth(document.getElementById("myHealthBar"), 0);
+   
+    const lftButton = document.getElementById("leftButton");
+    lftButton.addEventListener("click", function addhealth(){
+        document.querySelector(".health-bar-fill").style.width = `${100}%`;
+    })
+    
+
+
+    //Your pets happiness!
+    function updateHappy(happyBar,value) {
+        const happyBarFill = happyBar.querySelector(".happy-bar-fill");
+    
+    const stages = happyBarFill.dataset.stages
+        .split(",")
+        .map((stage) => stage.split(":"));
+    
+        stages.sort((stageA, stageB) => {
+            return stageA[0] - stageB[0]
+        });
+    
+        happyBarFill.style.width = `${value}%`;
+    }
+    
+        updateHappy(document.getElementById("myHappyBar"), 0);
+
+        const rghtButton = document.getElementById("rightButton");
+        rghtButton.addEventListener("click", function addhealth(){
+            document.querySelector(".happy-bar-fill").style.width = `${100}%`;
+        })
+
+
+
+        
